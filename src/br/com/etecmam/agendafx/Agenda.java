c2109c8e598c6fa8318bf28e1cfe64374de718a7
Aloisio Pinto <aloisiop1@gmail.com>
2017-05-29 17:54:16 -0300

    AGENDA - XML

--- src/br/com/etecmam/agendafx/Agenda.java
+++ src/br/com/etecmam/agendafx/Agenda.java
@@ -1,6 +1,9 @@
 package br.com.etecmam.agendafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Agenda extends Application {
@@ -9,9 +12,18 @@ public class Agenda extends Application {
 		launch(args);
 	}
 
-	@Override
-	public void start(Stage arg0) throws Exception {
 	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load( 
+				getClass().getResource("/br/com/etecmam/agendafx/Tela.fxml") );
+		Scene cena = new Scene(root);
+		
+		primaryStage.setScene(cena);
+		primaryStage.setTitle("AGENDA");
+		primaryStage.show();		
 		
 	}
 
