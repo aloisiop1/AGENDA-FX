e0760e8cdcda56437c57a7024774f372e4136eba
Aloisio Pinto <aloisiop1@gmail.com>
2017-05-29 17:49:39 -0300

    commit inicial

---
+++ src/br/com/etecmam/agendafx/Agenda.java
@@ -0,0 +1,21 @@
+package br.com.etecmam.agendafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Agenda extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+	
+		
+	}
+
+}
+
+
+	
